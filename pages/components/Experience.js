96e12df99372f112ebaaca7f146a57d3a69b1e71
kvadratikk <adahiter@gmail.com>
2023-03-29 17:46:54 +0300

    fix: experinece data

--- pages/components/Experience.js
+++ pages/components/Experience.js
@@ -1,38 +1,4 @@
-const items = [
-  {
-    date: [new Date('2021-08'), new Date('2021-09')],
-    position: 'Student of JS/FE Pre-School Course',
-    place: 'The Rolling Scopes School',
-    certificate: 'https://app.rs.school/certificate/wk2p6gwx',
-  },
-  {
-    date: [new Date('2021-10'), new Date('2022-04')],
-    position: 'Student of ST 2021 Q3 Course',
-    place: 'The Rolling Scopes School',
-    certificate: 'https://app.rs.school/certificate/i7osa4k0',
-  },
-  {
-    date: [new Date('2022-03'), new Date('2022-06')],
-    position: 'Student of REACT 2022 Q1 Course',
-    place: 'The Rolling Scopes School',
-    certificate: 'https://app.rs.school/certificate/76gqkpsr',
-  },
-  {
-    date: [new Date('2021-03'), new Date('2021-04')],
-    position: 'Student of JS/FE 2021 Q1 Course',
-    place: 'The Rolling Scopes School',
-  },
-  {
-    date: [new Date('2022-04'), 'currently'],
-    position: 'Student',
-    place: 'EPAM JS LAB',
-  },
-  {
-    date: [new Date('2022-07'), new Date('2022-10')],
-    position: 'Full-stack developer',
-    place: 'ALTRP',
-  },
-]
+import { ExperienceItems } from '../data/experience-items'
 
 const transformMonth = (month) => {
   return String(month).length < 2 ? '0' + month : month
@@ -47,11 +13,7 @@ const ExperienceItem = ({ date, position, place, certificate }) => {
             '/' +
             date[0].getFullYear() +
             ' – ' +
-            (typeof date[1] === 'string'
-              ? date[1]
-              : transformMonth(date[1].getMonth() + 1) +
-                '/' +
-                date[1].getFullYear())}
+            (typeof date[1] === 'string' ? date[1] : transformMonth(date[1].getMonth() + 1) + '/' + date[1].getFullYear())}
         </span>
         <span className='experience__arrow'></span>
       </p>
@@ -63,12 +25,7 @@ const ExperienceItem = ({ date, position, place, certificate }) => {
         <h3 className='experience__position'>{position}</h3>
         <h4 className='experience__place'>{place}</h4>
         {certificate && (
-          <a
-            className='experience__certificate'
-            href={certificate}
-            target='_blank'
-            rel='noreferrer'
-          >
+          <a className='experience__certificate' href={certificate} target='_blank' rel='noreferrer'>
             Certificate
           </a>
         )}
@@ -84,19 +41,9 @@ const Experience = () => {
         <h2>Experience</h2>
         <div className='experience__timeline'>
           <ul className='experience__list'>
-            {items
-              .sort((a, b) => b.date[0] - a.date[0])
-              .map((item, idx) => {
-                return (
-                  <ExperienceItem
-                    date={item.date}
-                    place={item.place}
-                    position={item.position}
-                    certificate={item.certificate}
-                    key={idx}
-                  />
-                )
-              })}
+            {ExperienceItems.sort((a, b) => b.date[0] - a.date[0]).map((item, idx) => {
+              return <ExperienceItem date={item.date} place={item.place} position={item.position} certificate={item.certificate} key={idx} />
+            })}
           </ul>
         </div>
       </div>
--- pages/components/Portfolio.js
+++ pages/components/Portfolio.js
@@ -1,102 +1,9 @@
 import { useState, useEffect } from 'react'
 import PortfolioItem from './PortfolioItem'
 
-import momentum from '../../assets/images/portfolio/momentum.webp'
-import artquiz from '../../assets/images/portfolio/artquiz.webp'
-import memories from '../../assets/images/portfolio/memories.webp'
-import game from '../../assets/images/portfolio/game.webp'
-import artshop from '../../assets/images/portfolio/artshop.webp'
-import pong from '../../assets/images/portfolio/pong.webp'
-import player from '../../assets/images/portfolio/player.webp'
-import slider from '../../assets/images/portfolio/slider.webp'
-import todo from '../../assets/images/portfolio/todo.webp'
-import three from '../../assets/images/portfolio/three.webp'
+import { useWindowSize } from '../hooks/useWindowSize'
 
-const items = [
-  {
-    name: 'portal',
-    link: 'https://lustrous-salamander-16e240.netlify.app/',
-    src: three,
-    date: new Date('2022-10-19'),
-    stack: ['react', 'react-three', 'three.js'],
-  },
-  {
-    name: 'simple game',
-    link: 'https://willowy-elf-a25b99.netlify.app/',
-    src: game,
-    date: new Date('2022-10'),
-    stack: ['canvas', 'ts', 'gsap'],
-  },
-  {
-    name: 'memories',
-    link: 'https://incomparable-trifle-4b8624.netlify.app/',
-    src: memories,
-    date: new Date('2022-06'),
-    stack: ['react', 'redux', 'axios', 'node', 'express', 'jwt', 'mongo', 'mui'],
-  },
-  {
-    name: 'art shop',
-    link: 'https://rolling-scopes-school.github.io/kvadratikk-REACT2022Q1/',
-    src: artshop,
-    date: new Date('2022-05'),
-    stack: ['react', 'ts', 'redux-toolkit', 'jest'],
-  },
-  {
-    name: 'angular to-do list',
-    link: 'https://rolling-scopes-school.github.io/kvadratikk-ST2021Q3/',
-    src: todo,
-    date: new Date('2022-02'),
-    stack: ['angular', 'ts', 'rxjs', 'scss'],
-  },
-  {
-    name: 'art quiz',
-    link: 'https://rolling-scopes-school.github.io/kvadratikk-JSFE2021Q3/art-quiz/',
-    src: artquiz,
-    date: new Date('2021-11'),
-    stack: ['ts', 'webpack', 'scss'],
-  },
-  {
-    name: 'momentum',
-    link: 'https://rolling-scopes-school.github.io/kvadratikk-JSFE2021Q3/momentum/',
-    src: momentum,
-    date: new Date('2021-10'),
-    stack: ['scss', 'js'],
-  },
-  {
-    name: 'video player',
-    link: 'https://rolling-scopes-school.github.io/kvadratikk-JSFEPRESCHOOL/custom-video-player/',
-    src: player,
-    date: new Date('2021-08'),
-    stack: ['sass', 'js'],
-  },
-  {
-    name: 'slider',
-    link: 'https://rolling-scopes-school.github.io/kvadratikk-JSFEPRESCHOOL/vertical-slider/',
-    src: slider,
-    date: new Date('2021-08'),
-    stack: ['sass', 'js'],
-  },
-  {
-    name: 'ping pong',
-    link: 'https://kvadratikk.github.io/pong-canvas/',
-    src: pong,
-    date: new Date('2021-06'),
-    stack: ['canvas', 'js'],
-  },
-]
-
-function useWindowSize() {
-  const [size, setSize] = useState([0, 0])
-  useEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight])
-    }
-    window.addEventListener('resize', updateSize)
-    updateSize()
-    return () => window.removeEventListener('resize', updateSize)
-  }, [])
-  return size
-}
+import { PortfolioItems } from '../data/portfolio-items'
 
 const Portfolio = () => {
   const [startIdx, setStartIdx] = useState(0)
@@ -119,8 +26,7 @@ const Portfolio = () => {
       <div className='container'>
         <h2>Portfolio</h2>
         <ul className='portfolio__list' style={{ opacity }}>
-          {items
-            .slice(0 + startIdx, startIdx + endIdx)
+          {PortfolioItems.slice(0 + startIdx, startIdx + endIdx)
             .sort((a, b) => b.date - a.date)
             .map((item) => {
               const { name, link, src, date, stack } = item
@@ -155,7 +61,7 @@ const Portfolio = () => {
             onClick={() => {
               setOpacity(0)
               setTimeout(() => {
-                startIdx + endIdx < items.length ? setStartIdx(startIdx + endIdx) : setStartIdx(0)
+                startIdx + endIdx < PortfolioItems.length ? setStartIdx(startIdx + endIdx) : setStartIdx(0)
                 setOpacity(1)
               }, 300)
             }}
